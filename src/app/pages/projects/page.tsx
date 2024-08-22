4adad7ede7f348cee7c9a02795fc9ea8200052cb
Seng Seang Leng <seangleng507@gmail.com>
2024-08-22 09:21:27 +0700

    adding seo

--- src/app/pages/projects/page.tsx
+++ src/app/pages/projects/page.tsx
@@ -1,5 +1,12 @@
 import TabsPages from "@/components/tabs/Tabs";
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "PROJECTS | JENZY",
+  description: "Completely projects",
+  
+};
 
 export default function ProjectsPage() {
   return <TabsPages value="projects" />;
